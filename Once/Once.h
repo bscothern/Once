5eeaaa95297fa4773146b3ae2057e59e38dc4809
Braden.Scothern <BradenScothern@gmail.com>
2017-10-11 22:42:55 -0600

    init commit

---
+++ Once/Once.h
@@ -0,0 +1,19 @@
+//
+//  Once.h
+//  Once
+//
+//  Created by Braden Scothern on 10/11/17.
+//  Copyright © 2017 DanceToaster LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Once.
+FOUNDATION_EXPORT double OnceVersionNumber;
+
+//! Project version string for Once.
+FOUNDATION_EXPORT const unsigned char OnceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Once/PublicHeader.h>
+
+
